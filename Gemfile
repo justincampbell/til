c8d0c9a88a3d2b7f40d003a913e9a2f95963f075
Justin Campbell <justin@justincampbell.me>
2014-09-15 17:33:00 -0400

    Copy layout, setup helpers

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 ruby '2.1.2'
 
 gem 'middleman'
+gem 'middleman-blog'
+gem 'slim'
--- config.rb
+++ config.rb
@@ -1,72 +1,46 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
-# Build-specific configuration
+set :slim, layout_engine: :slim
+
+# page '/feed.xml', layout: false
+
+activate :blog do |blog|
+  # blog.layout = "post.html"
+  # blog.permalink = "{date}/{slug}"
+  # blog.publish_future_dated = true
+  # blog.sources = "episodes/{episode}.html"
+  # blog.tag_template = "tag.html"
+  # blog.taglink = "tag/{tag}"
+end
+
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :asset_hash, exts: %w[css js]
+  activate :directory_indexes
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+end
 
-  # Minify Javascript on build
-  # activate :minify_javascript
+activate :google_analytics do |ga|
+  ga.tracking_id = 'UA-5973800-7'
+end
 
-  # Enable cache buster
-  # activate :asset_hash
+helpers do
+  def feedburner_url
+    "http://feeds.feedburner.com/justincampbell/til"
+  end
 
-  # Use relative URLs
-  # activate :relative_assets
+  def site_name
+    "Today, I Learned..."
+  end
 
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  def things
+    blog.articles
+  end
+
+  def title
+    current_page.data.title || site_name
+  end
 end
