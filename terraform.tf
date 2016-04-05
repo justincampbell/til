246edae8cb34a4b1ec21ecc72ba0c3d768842ac6
Justin Campbell <justin@justincampbell.me>
2016-04-05 12:14:50 -0400

    Use terraform-s3-website module

--- terraform.tf
+++ terraform.tf
@@ -1,48 +1,6 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-variable "host" {
-  default = "til"
-}
-
-variable "domain" {
-  default = "justincampbell.me"
-}
-
-variable "bucket" {
-  default = "til.justincampbell.me"
-}
-
-resource "dnsimple_record" "til" {
-  domain = "${var.domain}"
-  name   = "${var.host}"
-  type   = "CNAME"
-  value  = "${aws_s3_bucket.default.website_endpoint}"
-}
-
-resource "aws_s3_bucket" "default" {
-  bucket = "${var.bucket}"
-  acl    = "public-read"
-  policy = "${template_file.policy.rendered}"
-
-  logging {
-    target_bucket = "${var.bucket}-logs"
-  }
-
-  website {
-    index_document = "index.html"
-  }
-}
-
-resource "aws_s3_bucket" "logs" {
-  bucket = "${var.bucket}-logs"
-}
-
-resource "template_file" "policy" {
-  template = "${file("${path.module}/policy.json")}"
-
-  vars {
-    bucket = "${var.bucket}"
-  }
+module "terraform-s3-website" {
+  source = "github.com/justincampbell/terraform-s3-website"
+  bucket = "til.justincampbell.me"
+  domain = "justincampbell.me"
+  host   = "til"
 }
